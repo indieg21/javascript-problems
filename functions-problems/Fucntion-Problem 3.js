da7c76b4817409f22d9cea55d0a3c3b25ce39a83
indieg21 <Indiagraham21@gmail.com>
2021-09-05 12:21:03 -0400

    Problem 3

---
+++ functions-problems/Fucntion-Problem 3.js
@@ -0,0 +1,27 @@
+
+/*
+Write a JavaScript function that
+accepts two arguments, a string and a letter and the function will 
+count the number of occurrences of the specified letter within the string. 
+*/
+
+function countStr(str, a) {
+  
+    let arr = str.split("")
+    
+    let count = 0;
+    
+    for (let i = 0; i < arr.length; i++) {
+    
+      let char = arr[i]
+      if(char === a){
+      count += 1
+        
+        
+      }
+    
+  }
+    return count;
+  }
+  console.log(countStr("Woooooooooord", "o"))
+  
