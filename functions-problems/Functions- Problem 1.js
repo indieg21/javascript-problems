05f997d98afb3ee4c5e41680eb5c0a6ef442e7b2
indieg21 <Indiagraham21@gmail.com>
2021-09-05 11:56:09 -0400

    Count Vowels

---
+++ functions-problems/Functions- Problem 1.js
@@ -0,0 +1,31 @@
+/*
+step 1: define a JavaScript Function that accepts a string as a parmeter 
+step 2: find the longest word within the string.
+step 3: use the split method and a "for" loop.
+step 4: create the split giving a vaule of 0
+step 5: create a for loop to loop index with using .lenth as longestWord.length 
+step 6: return longest word
+step 7: run log 
+
+
+*/
+
+
+function findTheLongestWord(str) {
+    let words = str.split(' ')
+    let longestWord = "";
+    
+    for (let i = 0; i < words.length; i++) {
+      
+      if(words[i].length > longestWord.length){
+        
+        longestWord = words[i];
+      }
+    }
+    return longestWord;
+    
+    
+    
+  }
+  console.log(findTheLongestWord('I love furry puppies.' ));
+    
---
+++ functions-problems/Functions-Problems 2.js
@@ -0,0 +1,36 @@
+
+/* Write a JavaScript function that accepts a string as a parameter and
+//returns the number of vowels in  that string.
+/*
+step 1: define a JavaScript function that accepts a string as a parameter
+step 2: split the string passed in, turn it into an array of each individual letter,
+step 3: define a variable called count and set the inital value to be equal to 0;
+step 4: define a variable that defines what a vowel is. an array of vowels
+step 5: start looping through our array of letters
+step 6: check if the current letter in our loop is a vowel
+*/
+
+function countVowels(str) {
+    let newStr = str.toLowerCase()
+    let arr = newStr.split("")
+  
+    let vowel = ["a", "e", "i", "o", "u" ]; 
+    
+    let count = 0;
+    
+    for (let i = 0; i <arr.length; i++) {
+   
+    let char = arr[i]
+  
+    
+    if (vowel.includes(char)) {
+    count = count +1
+  }
+  }
+  
+    return count;
+  }
+  console.log(countVowels("Beautiful day todaAAAy"))
+  
+
+
