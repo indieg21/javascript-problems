100d7281ff242144adeb978c04d1210c3de2d841
indieg21 <Indiagraham21@gmail.com>
2021-09-06 13:08:28 -0400

    function problem 5

--- functions-problems/Fucntion-Problem 3.js
+++ functions-problems/Fucntion-Problem 3.js
@@ -3,6 +3,9 @@
 Write a JavaScript function that
 accepts two arguments, a string and a letter and the function will 
 count the number of occurrences of the specified letter within the string. 
+
+step 1: 
+
 */
 
 function countStr(str, a) {
--- functions-problems/Function-Problem 4.js
+++ functions-problems/Function-Problem 4.js
@@ -3,6 +3,10 @@
 Write a JavaScript function that checks whether a passed string is palindrome or not.
 A palindrome is a word, phrase, or sequence that reads the same 
 backward as forward, e.g., madam or nurses run.
+
+step 1: write fucntion that passes string 
+step 2: write a variable for empty string
+step 3: write a for loop for reversing string
 */
 
 function palindrome(str) {
