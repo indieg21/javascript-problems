a9552a5daafc06aacc33c4fe9138b19c4ff7c721
indieg21 <Indiagraham21@gmail.com>
2021-09-07 00:06:39 -0400

    Correct spaceing

--- functions-problems/Functions- Problem 1.js
+++ functions-problems/Functions- Problem 1.js
@@ -10,16 +10,14 @@ step 7: run log
 
 */
 
-
 function findTheLongestWord(str) {
-    let words = str.split(' ')
-    let longestWord = "";
-    for (let i = 0; i < words.length; i++) {
-      if(words[i].length > longestWord.length){
-        longestWord = words[i];
-      }
+  let words = str.split(" ");
+  let longestWord = "";
+  for (let i = 0; i < words.length; i++) {
+    if (words[i].length > longestWord.length) {
+      longestWord = words[i];
     }
-    return longestWord;
   }
-  console.log(findTheLongestWord('I love furry puppies.' ));
-    
+  return longestWord;
+}
+console.log(findTheLongestWord("I love furry puppies."));
