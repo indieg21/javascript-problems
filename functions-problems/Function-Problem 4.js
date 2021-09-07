c238b68091cb394f67903976abeaa2a389ba90a7
indieg21 <Indiagraham21@gmail.com>
2021-09-07 00:28:57 -0400

    Correct format

--- functions-problems/Function-Problem 4.js
+++ functions-problems/Function-Problem 4.js
@@ -1,4 +1,3 @@
-
 /*
 Write a JavaScript function that checks whether a passed string is palindrome or not.
 A palindrome is a word, phrase, or sequence that reads the same 
@@ -10,16 +9,14 @@ step 3: write a for loop for reversing string
 */
 
 function palindrome(str) {
-  
-    let reverse = ""
-    for (let i = 0; i < str.length; i++) { 
-    reverse = str[i]+reverse  
+  let reverse = "";
+  for (let i = 0; i < str.length; i++) {
+    reverse = str[i] + reverse;
   }
-    if(reverse === str){  
-        return true;
-      }
-    else{
-      return false;
-    }
+  if (reverse === str) {
+    return true;
+  } else {
+    return false;
   }
-  console.log(palindrome("madam"))
+}
+console.log(palindrome("madam"));
